09fcdab3dd64609c78839af00ab72d6b00143e43
AyushyaChongder <ayushyachongder@gmail.com>
2024-08-07 13:18:40 +0530

    first commit

---
+++ app/src/main/java/ac/project/myrecipeapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package ac.project.myrecipeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ac.project.myrecipeapp.ui.theme.MyRecipeAppTheme
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            MyRecipeAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ){
+
+                }
+            }
+        }
+    }
+}
+
